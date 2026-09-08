00cd1b2b0b8ced44b05c79f9fd58cb58d6532f06
kelechi-jesil-samuel <ksamuel9@byupathway.edu>
2026-09-08 13:23:16 -0700

    adding humburger menu

--- week02/scripts/temple.js
+++ week02/scripts/temple.js
@@ -4,3 +4,12 @@ document.getElementById("year").textContent = currentYear;
 //last modified date
 const lastModified = document.lastModified;
 document.getElementById("last-modified").textContent = lastModified;
+
+// hamburger menu toggle
+const menuButton = document.getElementById("menu-button");
+const navMenu = document.getElementById("nav-menu");
+
+menuButton.addEventListener("click", () => {
+  navMenu.classList.toggle("open");
+  console.log("menu clicked");
+});
